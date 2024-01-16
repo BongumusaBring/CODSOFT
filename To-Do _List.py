00f899d50aeb234d184955421a0ba4ac74fa8448
Bongumusa <bongumusam78@gmail.com>
2024-01-16 16:41:09 +0200

    To-Do_List

---
+++ To-Do _List.py
@@ -0,0 +1,9 @@
+import os
+
+def show_menu():
+    print("1. Show To-Do List")
+    print("2. Add Task")
+    print("3. Update Task")
+    print("4. Mark Task as Done")
+    print("5. Delete Task")
+    print("6. Exit")
