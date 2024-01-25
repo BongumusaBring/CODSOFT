2c40cd165bbff445554411bcced538ed2129ea1f
Bongumusa <bongumusam78@gmail.com>
2024-01-25 16:48:38 +0200

    List, Calculator

--- Task1_To-DoList/To-Do_List.py
+++ Task1_To-DoList/To-Do_List.py
@@ -12,7 +12,7 @@ def show_menu():
 
 def show_todo_list():
     try:
-        with open("todo.txt", "r") as file:
+        with open("Task1_To-DoList/todo.txt", "r") as file:
             tasks = file.readlines()
             if not tasks:
                 print("To-Do List is empty.")
@@ -26,7 +26,7 @@ def show_todo_list():
 
 def add_task():
     task = input("Enter the task: ")
-    with open("todo.txt", "a") as file:
+    with open("Task1_To-DoList/todo.txt", "a") as file:
         file.write(task + "\n")
     print("Task added successfully.")
 
@@ -34,12 +34,12 @@ def update_task():
     show_todo_list()
     try:
         task_index = int(input("Enter the task number to update: ")) - 1
-        with open("todo.txt", "r") as file:
+        with open("Task1_To-DoList/todo.txt", "r") as file:
             tasks = file.readlines()
             if 0 <= task_index < len(tasks):
                 new_task = input("Enter the updated task: ")
                 tasks[task_index] = new_task + "\n"
-                with open("todo.txt", "w") as file:
+                with open("Task1_To-DoList/todo.txt", "w") as file:
                     file.writelines(tasks)
                 print("Task updated successfully.")
             else:
@@ -51,11 +51,11 @@ def mark_task_done():
     show_todo_list()
     try:
         task_index = int(input("Enter the task number to mark as done: ")) - 1
-        with open("todo.txt", "r") as file:
+        with open("Task1_To-DoList/todo.txt", "r") as file:
             tasks = file.readlines()
             if 0 <= task_index < len(tasks):
                 del tasks[task_index]
-                with open("todo.txt", "w") as file:
+                with open("Task1_To-DoList/todo.txt", "w") as file:
                     file.writelines(tasks)
                 print("Task marked as done.")
             else:
@@ -67,11 +67,11 @@ def delete_task():
     show_todo_list()
     try:
         task_index = int(input("Enter the task number to delete: ")) - 1
-        with open("todo.txt", "r") as file:
+        with open("Task1_To-DoList/todo.txt", "r") as file:
             tasks = file.readlines()
             if 0 <= task_index < len(tasks):
                 del tasks[task_index]
-                with open("todo.txt", "w") as file:
+                with open("Task1_To-DoList/todo.txt", "w") as file:
                     file.writelines(tasks)
                 print("Task deleted successfully.")
             else:
